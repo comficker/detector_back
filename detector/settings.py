934327c2febc48c84c9486e644b50e22dd450a6f
Hoàng Anh Lâm <comficker@gmail.com>
2022-10-05 16:11:27 +0700

    Init

--- detector/settings.py
+++ detector/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -96,8 +98,12 @@ WSGI_APPLICATION = 'detector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DBNAME', 'db_name'),
+        'USER': os.getenv('POSTGRES_USER', 'db_use'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'db_pwd'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
